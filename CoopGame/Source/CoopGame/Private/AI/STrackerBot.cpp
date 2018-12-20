86487cce20a0ad3bba7e2b593807ecfdda2f2f75
Brent Caulfield <brentcaulfield@gmail.com>
2018-12-20 09:10:54 -0500

    Lectures 93-94: Adding Move-To logic using FindPathToActorSynchronously to find a path to the player with a function GetNextPathPoint which returns an FVector that is the next path point. Then the AI is moved using AddForce on it's mesh in the normalized direction from the NextPathPoint minus it's current location. Once it reaches a minimum distance it then gets the next path point and repeats effectively chasing the player around the map.

--- CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
+++ CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -2,6 +2,10 @@
 
 #include "AI/STrackerBot.h"
 #include "Components/StaticMeshComponent.h"
+#include "NavigationSystem/Public/NavigationSystem.h"
+#include "NavigationSystem/Public/NavigationPath.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ASTrackerBot::ASTrackerBot()
@@ -11,7 +15,12 @@ ASTrackerBot::ASTrackerBot()
 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));	
 	MeshComp->SetCanEverAffectNavigation(false);
+	MeshComp->SetSimulatePhysics(true);
 	RootComponent = MeshComp;
+
+	bUseVelocityChange = false;
+	MovementForce = 1000;
+	RequiredDistanceToTarget = 100;
 }
 
 // Called when the game starts or when spawned
@@ -19,6 +28,25 @@ void ASTrackerBot::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// find initial move-to
+	NextPathPoint = GetNextPathPoint();
+}
+
+FVector ASTrackerBot::GetNextPathPoint()
+{	
+	// Hack, to get player location
+	auto PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+	
+	if (NavPath->PathPoints.Num() > 1)
+	{
+		// return next point in the path
+		return NavPath->PathPoints[1];
+	}
+
+	// Failed to find path
+	return GetActorLocation();
 }
 
 // Called every frame
@@ -26,5 +54,26 @@ void ASTrackerBot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	float DistanceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+	if (DistanceToTarget <= RequiredDistanceToTarget)
+	{
+		NextPathPoint = GetNextPathPoint();		
+
+		DrawDebugString(GetWorld(), GetActorLocation(), "Target Reached!");
+	}
+	else
+	{
+		// Keep moving towards next target
+		FVector ForceDirection = NextPathPoint - GetActorLocation();
+		ForceDirection.Normalize();
+		ForceDirection *= MovementForce;
+
+		MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
+
+		DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ForceDirection, 32, FColor::Yellow, false, 0.0f, 0, 1.0f);
+	}
+
+	DrawDebugSphere(GetWorld(), NextPathPoint, 20, 12, FColor::Yellow, false, 0.0f, 1.0f);
 }
 
