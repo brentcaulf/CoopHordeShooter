79d1b7350f93ac3e911df72353c9df8beb6c5ebc
Brent Caulfield <brentcaulfield@gmail.com>
2018-12-22 15:01:55 -0500

    Lecture 111: Created a HealthRegen powerup by adding a new public function to the HealthComponent called Heal and then calling it from the new BP_Powerup_HeathRegen

--- CoopGame/Source/CoopGame/Private/Components/SHealthComponent.cpp
+++ CoopGame/Source/CoopGame/Private/Components/SHealthComponent.cpp
@@ -53,6 +53,21 @@ void USHealthComponent::OnRep_Health(float OldHealth)
 	OnHealthChanged.Broadcast(this, Health, Damage, nullptr, nullptr, nullptr);
 }
 
+void USHealthComponent::Heal(float HealAmount)
+{
+	if (HealAmount <= 0.0f || Health <= 0.0f)
+	{
+		return;
+	}
+	
+
+	Health = FMath::Clamp(Health + HealAmount, 0.0f, DefaultHealth);
+
+	UE_LOG(LogTemp, Log, TEXT("%s Heals %s Health. New Health: %s"), *GetOwner()->GetName(), *FString::SanitizeFloat(HealAmount), *FString::SanitizeFloat(Health));
+
+	OnHealthChanged.Broadcast(this, Health, -HealAmount, nullptr, nullptr, nullptr);
+}
+
 void USHealthComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
