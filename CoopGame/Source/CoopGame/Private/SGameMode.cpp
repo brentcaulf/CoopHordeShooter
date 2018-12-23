ec5ed3e6c74df57c28b4746d5ceb49cfd2989000
Brent Caulfield <brentcaulfield@gmail.com>
2018-12-23 14:46:27 -0500

    Lecture 121: Adding Wave Based Spawning to the GameMode. StartPlay calls PrepareforNextWave which then sets a timer called NextWaveStart which then calls the StartWave function which then creates a NrOfBotsToSpawn based on the current WaveCount. StartWave then starts a timer which calls the SpawnBotTimerElapsed function every second which then calls a blueprint implementable event named SpawnNewBot where the actual bot spawning is handled in the BP_TestGameMode using the EQS system. Once the NrOfBotsToSpawn has been called the EndWave function is called which clears the BotSpawner timer and then calls the PrepareForNextWave function. Current implementation is a expontentially increasing spawning wave with no end.

--- CoopGame/Source/CoopGame/Private/SGameMode.cpp
+++ CoopGame/Source/CoopGame/Private/SGameMode.cpp
@@ -1,7 +1,51 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SGameMode.h"
+#include "TimerManager.h"
 
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2.0f;
+}
 
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
 
+	PrepareforNextWave();
+}
 
+void ASGameMode::PrepareforNextWave()
+{
+
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void ASGameMode::StartWave()
+{
+	WaveCount++;
+	
+	NrOfBotsToSpawn = 2 * WaveCount;
+	
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+}
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NrOfBotsToSpawn--;
+
+	if (NrOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareforNextWave();
+}
--- CoopGame/Source/CoopGame/Public/SGameMode.h
+++ CoopGame/Source/CoopGame/Public/SGameMode.h
@@ -13,8 +13,42 @@ UCLASS()
 class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
+
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	// Bots to spawn in current wave
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
 	
+protected:
+
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Set Timer for next start wave
+	void PrepareforNextWave();
+
+	// Start Spawning Bots
+	void StartWave();
+
+	// Stop Spawning Bots
+	void EndWave();
+
 	
-	
-	
+
 };
