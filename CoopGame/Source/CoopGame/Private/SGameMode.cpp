c9d0f4548f694291fd548b4b6845ee9c98acc0bf
Brent Caulfield <brentcaulfield@gmail.com>
2018-12-24 14:11:15 -0500

    Lecture 130: Respawning dead players in the game mode using a new function RestartDeadPlayers which is called in the PrepareForNextWave function and which iterates through the player controllers in the world and then if it finds any that don't have a pawn it calls a native function called RestartPlayer with a parameter of the player controller which then respawns that player with a new pawn.

--- CoopGame/Source/CoopGame/Private/SGameMode.cpp
+++ CoopGame/Source/CoopGame/Private/SGameMode.cpp
@@ -38,6 +38,8 @@ void ASGameMode::PrepareforNextWave()
 	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
 
 	SetWaveState(EWaveState::WaitingToStart);
+
+	RestartDeadPlayers();
 }
 
 void ASGameMode::StartWave()
@@ -146,4 +148,16 @@ void ASGameMode::SetWaveState(EWaveState NewState)
 		GS->SetWaveState(NewState);
 	}
 
+}
+
+void ASGameMode::RestartDeadPlayers()
+{
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
+	{
+		APlayerController* PC = It->Get();
+		if (PC && PC->GetPawn() == nullptr)
+		{
+			RestartPlayer(PC);
+		}
+	}
 }
