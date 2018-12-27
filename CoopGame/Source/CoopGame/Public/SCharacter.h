d8c013bcf8f25bed92e0e03db97946a071bf104e
Brent Caulfield <brentcaulfield@gmail.com>
2018-12-27 11:14:18 -0500

    Lecture 138: Exposing the StartFire and StopFire functions so that they can be called from a new AI Task in the advanced AI behavior tree.

--- CoopGame/Source/CoopGame/Public/SCharacter.h
+++ CoopGame/Source/CoopGame/Public/SCharacter.h
@@ -62,10 +62,6 @@ protected:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Player")
 	FName WeaponAttachSocketName;
 
-	void StartFire();
-
-	void StopFire();
-
 	UFUNCTION()
 	void OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
 
@@ -82,4 +78,9 @@ public:
 
 	virtual FVector GetPawnViewLocation() const override;
 	
+	UFUNCTION(BlueprintCallable, Category = "Player")
+	void StartFire();
+
+	UFUNCTION(BlueprintCallable, Category = "Player")
+	void StopFire();
 };
